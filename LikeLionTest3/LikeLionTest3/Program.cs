ac96ebfd551aedde9d8cff1f68ea032ebca4444c
Jino-Lee <90809030+Leetany@users.noreply.github.com>
2025-02-21 10:44:47 +0900

    study3 learning literal

---
+++ LikeLionTest3/LikeLionTest3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLionTest3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp = 100;
+            double att = 56.7;
+            string charactername = "Alice";
+            char rank = 'S';
+
+            Console.WriteLine($"HP: {hp}");
+            Console.WriteLine($"ATTACK: {att}");
+            Console.WriteLine($"Name: {charactername}");
+            Console.WriteLine($"Rank: {rank}");
+        }
+    }
+}

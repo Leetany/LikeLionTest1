16ab79d3fa282677b5e136ad4ba5ced7fefd0351
Jino-Lee <90809030+Leetany@users.noreply.github.com>
2025-02-21 12:14:25 +0900

    study3

---
+++ LikeLionTest4/LikeLionTest4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLionTest4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string greeting;
+            greeting = "Hello, World!";
+
+            Console.WriteLine(greeting);*/
+
+            /*int score = 100;
+            double temperature = 36.5;
+            string city = "Seoul";
+
+            Console.WriteLine(score);
+            Console.WriteLine(temperature);
+            Console.WriteLine(city);*/
+
+            /*int x = 10, y = 20, z = 30;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);*/
+
+            /*const double Pi = 3.14159;
+            const int MaxScore = 100;
+
+            Console.WriteLine("Pi: " + Pi);
+            Console.WriteLine("Max Score: " + MaxScore);*/
+
+            int att = 16755;
+            int MaxHP = 78103;
+            int Critical = 36;
+            int Specialization = 1017;
+            int Domination = 41;
+            int Swiftness = 611;
+            int Endurance = 22;
+            int Expertise = 39;
+
+            Console.WriteLine("공격력: " + att);
+            Console.WriteLine("최대 생명력: " + MaxHP);
+            Console.WriteLine("치명: " + Critical);
+            Console.WriteLine("특화: " + Specialization);
+            Console.WriteLine("제압: " + Domination);
+            Console.WriteLine("신속: " + Swiftness);
+            Console.WriteLine("인내: " + Endurance);
+            Console.WriteLine("숙련: " + Expertise);
+        }
+    }
+}
